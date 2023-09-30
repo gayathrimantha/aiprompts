7cb5fc9683409b3b4389d62d91382c9874edf3b8
Gayathri Mantha <gayathrimantha16@gmail.com>
2023-09-30 13:55:38 +0530

    db updates

--- shareprompts/app/api/auth/[...nextauth]/route.js
+++ shareprompts/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDb } from "@utils/database";
+import User from "@models/user";
+
 console.log({
   clientId: process.env.GOOGLE_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -14,7 +17,30 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDb();
+
+      //chec if user already exits
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      //if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLoweCase(),
+          image: profile.picture
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
